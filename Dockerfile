229ac0712336a3bac8df4739a0b7d5c6776472f4
Artyom Sosedka <sostema@gmail.com>
2022-08-10 15:58:37 +0300

    refactor: full refactor of code

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM python:3.7
-
-COPY . /app
-WORKDIR /app
-
-RUN pip install -r requirements.txt
-
-ENTRYPOINT python main.py
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from Code.TelegramAPI.telegram import main
+from src.tele.telegram import main
 
 if __name__ == "__main__":
     main()
